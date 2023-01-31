f6e72faf686d1032dfbf05133fdd0d5bdc1b5426
William McGonigal <44418542+willymcgeejr@users.noreply.github.com>
2023-01-30 21:53:23 -0500

    Update build.gradle and module-info with CSV and PostgreSQL Library Declarations

--- BudgetingProject/src/main/java/module-info.java
+++ BudgetingProject/src/main/java/module-info.java
@@ -3,9 +3,15 @@ module com.wmcg.budgetingproject {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires com.opencsv;
+
+    requires java.sql;
+
+    requires org.postgresql.jdbc;
 
     opens com.wmcg.budgetingproject to javafx.fxml;
     exports com.wmcg.budgetingproject;
     exports com.wmcg.budgetingproject.controller;
+    exports com.wmcg.budgetingproject.data to com.opencsv;
     opens com.wmcg.budgetingproject.controller to javafx.fxml;
 }
